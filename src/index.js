1d1c71e89f6e85e82f053272a12884df2f7c7984
Gabriela Leal <gc.lealb@gmail.com>
2023-03-27 19:16:10 -0300

    update validator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import validator from './validator.js';
 
-console.log(validator);
+// console.log(validator.isValid("4083952015263"));
+
+/* for, crea una variable con valor 0. Condicion que si es true, hace lo que está dentro del for.
+El i++ le suma 1 a la variable i. 
+*/
+
+// Poder ingresar el texto en el recuadro + boton con funcionalidad
+const boton = document.getElementById("boton")
+const text = document.getElementById("text")
+let tarjetaSinGato = ""
+boton.addEventListener("click", function(){
+  const input = document.getElementById("text");
+  const isValid = validator.isValid(input.value);
+  if (isValid) {
+    const alerta = document.getElementsByClassName("valida")
+    alerta[0].classList.remove("hidden")
+    const alerta1 = document.getElementsByClassName("invalida")
+    alerta1[0].classList.add("hidden")
+  }else{
+    const alerta = document.getElementsByClassName("invalida")
+    alerta[0].classList.remove("hidden")
+    const alerta1 = document.getElementsByClassName("valida")
+    alerta1[0].classList.add("hidden")
+  }
+  /* console.log(validator.isValid(input.value))
+  console.log(validator.maskify(input.value)) */
+})
+
+text.addEventListener("focusout", function(){
+  tarjetaSinGato = text.value
+  text.value = validator.maskify(text.value)
+})
+
+text.addEventListener("focusin", function(){
+  text.value = tarjetaSinGato
+})
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,59 @@
 const validator = {
-  // ...
+  isValid(numeroTarjeta) {
+
+    // pasar string a array
+    const arrayTarjeta = numeroTarjeta.split("")
+
+    // dar vuelta los numeros del array
+    const reversaArrayTarjeta = arrayTarjeta.reverse()
+
+    const arrayMultiplicado = []
+
+    // recorrer el array
+    for (let i = 0; i < reversaArrayTarjeta.length; i++) {
+      // verificar si la posición del número es impar (numero impar porque es un array y empieza en 0)
+      if (i % 2 === 0) {
+        arrayMultiplicado.push(Number(reversaArrayTarjeta[i]))
+      } else {
+        // multiplicar por 2 el numero impar
+        arrayMultiplicado.push(reversaArrayTarjeta[i] * 2)
+      }
+    }
+
+    // recorrer el array
+    for (let i = 0; i < arrayMultiplicado.length; i++) {
+      // verificar si tiene 2 digitos
+      if (arrayMultiplicado[i] > 9) {
+        // separar el numero, se suma y se asigna a la posicion correspondiente
+        const separarNumero = arrayMultiplicado[i].toString().split("")
+        arrayMultiplicado[i] = Number(separarNumero[0]) + Number(separarNumero[1])
+      }
+    }
+
+    // declarar una nueva variable para sumar los valores del array
+    let acumulado = 0
+    for (let i = 0; i < arrayMultiplicado.length; i++) {
+      acumulado = acumulado + arrayMultiplicado[i]
+    }
+    // retorna verdadero cuando es un multiplo de 10
+    return acumulado % 10 === 0
+  }, 
+  maskify(numeroTarjeta){
+    numeroTarjeta = numeroTarjeta.split("").reverse()
+    const acumulador = []
+    for(let i = 0; i < numeroTarjeta.length; i++){
+      if(i<4){
+        acumulador.push(numeroTarjeta[i])
+      }else{
+        acumulador.push("#")
+      }
+    }
+    return acumulador.reverse().join("")
+  }
 };
 
 export default validator;
+
+// 12345
+// [1,2,3,4,5] --> [5,4,3,2,1]
+// [5,4,3,2,#] --> [#,2,3,4,5] --> va a devolver string #2345
